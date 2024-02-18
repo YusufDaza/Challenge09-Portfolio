d3e834175a8df115e33e6a6d46dc09a5b8d80304
Joseph Daza <josephdazz@hotmail.com>
2024-02-17 20:26:24 -0500

    javascript updated

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,113 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use:'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide guidelines for others to contribute to your project:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide any tests written for your application and provide examples on how to run them:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('README.md successfully generated!');
+        }
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = `
+# ${answers.title}
+
+## Description
+
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## Contributing
+
+${answers.contributing}
+
+## Tests
+
+${answers.tests}
+
+## License
+
+This project is licensed under the ${answers.license} license.
+
+## Questions
+
+For questions about this project, please contact [${answers.githubUsername}](https://github.com/${answers.githubUsername}) at ${answers.email}.
+`;
+        writeToFile('README.md', readmeContent);
+    });
+}
 
 // Function call to initialize app
 init();
